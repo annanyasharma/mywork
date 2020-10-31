cbc7aa759fd77803377824ad4a7ab51201600850
Annanya <annanyasharma868@gmail.com>
2020-10-31 16:33:17 +0530

    Python Books and programs

---
+++ Python notes and question/#python/Invent with Python/Comp Files/hello2.py
@@ -0,0 +1,7 @@
+def sayHello(name):
+	print('Hello, ' + name)
+print('Say hello to Fegley.')
+fizzy = 'Fegley'
+sayHello(fizzy)
+print('Do not forget to fuck that faggot Bernock')
+sayHello('Bernock')
